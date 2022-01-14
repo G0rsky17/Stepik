5b585238df220cbe72d6110ceab4aa47d67705ef
unknown <Igor@C1000430.niivs.ru>
2022-01-14 13:47:18 +0300

    Initial commit

---
+++ src/com/company/Shape.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Shape {
+    private final Color color;
+    public Shape(Color color) {this.color = color;}
+    public Color getColor() {return color;}
+    public double getArea() {return Double.NaN;}
+}
